0c50a6808954e730fb7a9547ce4e82dd83089211
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2025-09-14 14:37:47 -0400

    Vue+tailwind+daisyui setup

---
+++ app/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="text-purple-600">Student Front End</div>
+    <button class="btn btn-primary">daisytest</button>
+  </div>
+</template>
+<script setup>
+let user = 'blahabahd'
+</script>
