75072bcb73d1d260403ffbb2de2b294784306c6d
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2025-09-17 10:39:55 -0400

    Remove old vue setup, add nuxt w/ tailwind & daisyui

---
+++ app/app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="btn btn-primary">Hello</h1>
+  </div>
+</template>
--- app/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <div class="text-purple-600">Student Front End</div>
-    <button class="btn btn-primary">daisytest</button>
-  </div>
-</template>
-<script setup>
-let user = 'blahabahd'
-</script>
