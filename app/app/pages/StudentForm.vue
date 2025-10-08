c6eb8f4b6f2d3b3be58eee3fca32fa1793bd9f18
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2025-10-08 08:45:17 -0400

    Added option to update existing form and logic for pulling, pushing, and updating via backend
    I missed my flight bruh
    realized i forgot to push so a bunch of stuff

--- app/app/pages/StudentForm.vue
+++ app/app/pages/StudentForm.vue
@@ -1,12 +1,54 @@
 <template>
   <div class="flex justify-center items-center min-h-screen bg-gray-500">
+    <button
+      @click="edittingForm = true"
+      class="absolute top-6 bg-primary px-4 py-2 rounded shadow hover:bg-black transition"
+      v-if="!edittingForm"
+    >
+      Want to edit a form? Click here
+    </button>
     <div
+      v-if="edittingForm && !loggedIn"
+      class="card w-full border-2 border-black max-w-md bg-white shadow-xl p-6 cursor-default mt-6"
+    >
+      <h1 class="text-black text-3xl font-bold text-center mb-6">
+        Login using your verification code:
+      </h1>
+      <form @submit.prevent="leaderLogin()">
+        <FormInput
+          category="First Name"
+          color="black"
+          v-model="loginUser.firstName"
+          type="text"
+          placeholder="Enter here"
+        />
+        <FormInput
+          category="Last Name"
+          color="black"
+          v-model="loginUser.lastName"
+          type="text"
+          placeholder="Enter here"
+        />
+        <FormInput
+          category="Verification Code"
+          color="black"
+          v-model="loginUser.verificationCode"
+          type="text"
+          placeholder="Enter here"
+        />
+        <button type="submit" class="btn btn-primary w-full mt-6">
+          Submit Login
+        </button>
+      </form>
+    </div>
+    <div
+      v-else
       class="card w-full border-2 border-black max-w-md bg-white shadow-xl p-6 cursor-default mt-6"
     >
       <h1 class="text-black text-3xl font-bold text-center mb-6">
         Student Form
       </h1>
-      <form @submit.prevent="submit">
+      <form @submit.prevent="submit()">
         <FormInput
           category="First Name"
           color="black"
@@ -58,7 +100,9 @@
               step="1"
               v-model.number="GroupSize"
               @input="organizeGroup()"
+              :disabled="loggedIn"
             />
+            <!-- MIGHT REMOVE :DISABLED, DEPENDS ON IF WE ALLOW ADDITIONAL STUDENTS TO BE ADDED/REMOVED FROM A GROUP AFTER SUBMISSION -->
             <div class="flex justify-between px-2.5 mt-2 text-xs">
               <span v-for="i in 11">|</span>
             </div>
@@ -114,6 +158,14 @@
 </template>
 
 <script lang="ts" setup>
+//(for me) make sure group slider's starting point is updated based on group size
+const edittingForm = ref(false);
+const loggedIn = ref(false);
+const loginUser = reactive({
+  firstName: "",
+  lastName: "",
+  verificationCode: "",
+});
 const groupLeader = reactive<Student>({
   firstName: "",
   lastName: "",
@@ -124,12 +176,24 @@ const InGroup = ref(false);
 const GroupSize = ref(1);
 const Group = ref<Student[]>([groupLeader]);
 
-definePageMeta({
-  middleware: "auth",
-});
-function dataCheck() {
-  //when excel is here check if input is in the data
-  //should also check if student is already in a group thats been submitted
+async function leaderLogin() {
+  const response = await fetch(""); //pull data from mongodb using backend
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const data = await response.json();
+  for (let i = 0; i < data.length; i++) {
+    if (
+      data[i].leader.first_name == loginUser.firstName &&
+      data[i].leader.last == loginUser.lastName &&
+      data[i].leader == loginUser.verificationCode //user still needs a verification code generated (via. mongodb)
+    ) {
+      loggedIn.value = true;
+      edittingForm.value = true;
+      return;
+    }
+  }
+  alert("Incorrect name and/or verification");
 }
 function organizeGroup() {
   if (!InGroup.value) {
@@ -150,11 +214,55 @@ function clearGroup() {
     Group.value = [groupLeader];
   }
 }
-function submit() {
-  console.log("Group:", Group.value);
-  //will push stuff to mongodb; log for now
-}
-function createVerificationCode() {
-  //will generate code for table leaders that want to make an edit to their group and show it to them
+async function submit() {
+  //make sure leaders and data are on mongodb **being done on backend? (if not i'll do it)**
+  const dataPush = {
+    leader: groupLeader,
+    members: Group.value.slice(1),
+  };
+  const osisCheck =
+    (groupLeader.osis as string).length === 9 &&
+    !isNaN(Number(groupLeader.osis));
+  const emailCheck = groupLeader.email.includes("@nycstudents.net");
+
+  if (!osisCheck || !emailCheck) {
+    alert("Enter a 9 digit OSIS and an @nycstudents.net email");
+    return;
+  }
+
+  if (edittingForm.value == true) {
+    try {
+      const res = await fetch("", {
+        //update data on MongoDB (using backend)
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataPush),
+      });
+      const data = await res.json();
+      console.log("Updated:", data);
+    } catch (err) {
+      alert("couldnt update data on mongodb");
+      console.log(err);
+    }
+  } else {
+    try {
+      const res = await fetch("", {
+        //push data here (using backend)
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataPush),
+      });
+      const data = await res.json();
+      console.log("Pushed:", data);
+    } catch (err) {
+      alert("couldnt push data to mongodb");
+      console.log(err);
+    }
+    console.log("Group:", Group.value);
+  }
 }
 </script>
