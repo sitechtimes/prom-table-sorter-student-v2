32af630d9d0774526320dcf0996f856bed8f68cd
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2026-01-14 10:30:09 -0500

    Adding and removing table function
    work from yesterday
    did some brainstorming on how to make the visualizer work with more tables (ex., 20+)

--- app/app/components/TableVisualizer.vue
+++ app/app/components/TableVisualizer.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="space-y-6">
+    <button class="btn" @click="addTable(tables[0]!.capacity)">
+      Add Table
+    </button>
     <div
       v-for="(table, i) in tables"
       :key="i"
       class="p-4 bg-white rounded-xl shadow"
     >
-      <h4 class="text-lg font-semibold text-gray-700 mb-2">
+      <div>
+        <button class="btn btn-red-600" @click="removeTable(table, i)">
+          Remove Table
+        </button>
+      </div>
+      <h4 class="text-lg font-bold text-gray-700 mb-3">Table: {{ i }}</h4>
+      <h5 class="text-md font-semibold text-gray-700 mb-2" :key="i">
         Free seats: {{ table.unoccupiedSeats }}
-      </h4>
 
       <Draggable
         v-model="table.occupants"
@@ -39,20 +47,15 @@
 </template>
 
 <script setup lang="ts">
-//next to work on: display name and email as well as make it so free seats left updates on the table
+//next to work on: display name and email (basically just use dropdown from the other table) as well as make it so free seats left updates on the table
+//fix styling
+//potentially group together all the single student groups for better visibility with large amounts of table. IDEA: MAKE EACH TABLE A DROP DOWN
 import Draggable from "vuedraggable-esm";
 const props = defineProps<{
   tables: Table[];
 }>();
 
 const tables = props.tables;
-function calculateSeats(table: Table): number {
-  const occupiedSeats = table.occupants.reduce(
-    (sum, group) => sum + group.members.length + 1,
-    0
-  );
-  return table.capacity - occupiedSeats;
-}
 function moveable(
   evt: {
     draggedContext: { element: Group };
@@ -65,7 +68,7 @@ function moveable(
   const incomingSeats = group.members.length + 1;
 
   const occupiedAfterIncomingGroup = evt.relatedContext.list.reduce(
-    (sum, g) => sum + g.members.length + 1,
+    (sum, group) => sum + group.members.length + 1,
     0
   );
 
@@ -74,6 +77,19 @@ function moveable(
   return incomingSeats <= remainingSeats;
   //potential option to remove a table?
   //maybe make it moveable but an alert will pop up indicating the table size is beyond capacity
-  return group.members.length + 1 <= remainingSeats;
+}
+function addTable(tableCapacity: number) {
+  tables.push({
+    capacity: tableCapacity,
+    unoccupiedSeats: tableCapacity,
+    occupants: [],
+  });
+}
+function removeTable(table: Table, tableIndex: number) {
+  if (table.occupants.length == 0) {
+    tables.splice(tableIndex, 1);
+  } else {
+    alert("Can not remove table unless the table is empty");
+  }
 }
 </script>
