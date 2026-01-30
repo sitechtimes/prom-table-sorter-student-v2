4b6c93d29650194a71df46eb30d161428cc310a1
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2026-01-30 11:02:26 -0500

    changes
    styling changes and unnecessary stuff removed

--- app/app/components/landing.vue
+++ app/app/components/landing.vue
@@ -1,45 +1,36 @@
 <template>
   <div class="h-screen">
     <div
-      class="grid grid-cols-1 md:grid-cols-3 space-y-2 place-items-center h-7/8 md:h-3/4"
+      class="flex flex-col sm:flex-row sm:justify-between items-center h-7/8 sm:h-3/4 max-w-7xl mx-auto px-6 py-12 sm:py-20 space-y-8 sm:space-y-0"
     >
-      <div class="card bg-base-100 w-7/8 h-11/12 shadow-sm">
-         <figure class="h-5/8">
-             <img src="/flow-chart-svgrepo-com.svg" class="h-1/2 hidden md:block"/>
-          </figure>
+      <div
+        class="card bg-base-100 shadow-sm w-72 h-96 sm:w-[32rem] sm:h-[40rem]"
+      >
+        <figure class="h-1/3 sm:h-5/8 flex items-center justify-center">
+          <img src="/student-card-svgrepo-com.svg" class="h-24 sm:h-1/2" />
+        </figure>
         <div class="card-body">
-          <h2 class="card-title font-bold md:text-2xl">Visualizer</h2>
-          <p>To be added.</p>
-          <div
-            class="card-actions justify-end underline font-semibold md:text-xl"
-          >
-            <NuxtLink to="/visualizer">Go To</NuxtLink>
-          </div>
-        </div>
-      </div>
-      <div class="card bg-base-100 w-7/8 h-11/12 shadow-sm">
-        <figure class="h-5/8">
-               <img src="/student-card-svgrepo-com.svg" class="h-1/2 hidden md:block"/>
-          </figure>
-        <div class="card-body">
-          <h2 class="card-title font-bold md:text-2xl">Submit Your Group</h2>
+          <h2 class="card-title font-bold sm:text-2xl">Submit Your Group</h2>
           <p>Students, go here to make your prom groups.</p>
           <div
-            class="card-actions justify-end underline font-semibold md:text-xl"
+            class="card-actions justify-end underline font-semibold sm:text-xl"
           >
             <NuxtLink to="/studentForm">Go To</NuxtLink>
           </div>
         </div>
       </div>
-      <div class="card bg-base-100 w-7/8 h-11/12 shadow-sm">
-        <figure class="h-5/8">
-             <img src="/admin-svgrepo-com.svg" class="h-1/2 hidden md:block"/>
-          </figure>
+      <div
+        class="card bg-base-100 shadow-sm w-72 h-96 sm:w-[32rem] sm:h-[40rem]"
+      >
+        <figure class="h-1/3 sm:h-5/8 flex items-center justify-center">
+          <img src="/admin-svgrepo-com.svg" class="h-24 sm:h-1/2" />
+        </figure>
+
         <div class="card-body">
-          <h2 class="card-title font-bold md:text-2xl">Admin View</h2>
+          <h2 class="card-title font-bold sm:text-2xl">Admin View</h2>
           <p>Administrator, click here to go to the admin log in page.</p>
           <div
-            class="card-actions justify-end underline font-semibold md:text-xl"
+            class="card-actions justify-end underline font-semibold sm:text-xl"
           >
             <NuxtLink to="/adminLogin">Go To</NuxtLink>
           </div>
@@ -48,7 +39,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
--- app/app/components/navbar.vue
+++ app/app/components/navbar.vue
@@ -23,17 +23,15 @@
           class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
         >
           <li><NuxtLink to="/">Home</NuxtLink></li>
-          <li><NuxtLink to="/visualizer">Visualizer</NuxtLink></li>
           <li><NuxtLink to="/studentForm">Make Groups</NuxtLink></li>
           <li><NuxtLink to="/studentEdit">Edit Groups</NuxtLink></li>
           <li><NuxtLink to="/adminLogin">Admin Login</NuxtLink></li>
         </ul>
       </div>
-      <!-- <a class="btn btn-ghost text-xl">SITHS Prom Table Sorter</a> -->
     </div>
     <div class="navbar-center">
       <NuxtLink to="/" class="btn btn-ghost text-xl"
-        >Prom Table Sorter</NuxtLink
+        >SITHS Prom Table Sorter</NuxtLink
       >
     </div>
     <div class="navbar-end">
