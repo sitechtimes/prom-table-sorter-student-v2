1dbadf5ca3daa801f659ca8180f7edc00a984a32
katherine <katherineze26@gmail.com>
2026-02-09 10:33:52 -0500

    jh

--- app/app/components/landing.vue
+++ app/app/components/landing.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="h-screen">
+  <div class="">
     <div
       class="flex flex-col sm:flex-row sm:justify-between items-center h-7/8 sm:h-3/4 max-w-7xl mx-auto px-6 py-12 sm:py-20 space-y-8 sm:space-y-0"
     >
       <div
-        class="card bg-base-100 shadow-sm w-72 h-96 sm:w-[32rem] sm:h-[40rem]"
+        class="card bg-base-100 shadow-sm w-72 sm:w-[32rem] sm:h-[40rem]"
       >
         <figure class="h-1/3 sm:h-5/8 flex items-center justify-center">
           <img src="/student-card-svgrepo-com.svg" class="h-24 sm:h-1/2" />
@@ -20,7 +20,7 @@
         </div>
       </div>
       <div
-        class="card bg-base-100 shadow-sm w-72 h-96 sm:w-[32rem] sm:h-[40rem]"
+        class="card bg-base-100 shadow-sm w-72 sm:w-[32rem] sm:h-[40rem]"
       >
         <figure class="h-1/3 sm:h-5/8 flex items-center justify-center">
           <img src="/admin-svgrepo-com.svg" class="h-24 sm:h-1/2" />
