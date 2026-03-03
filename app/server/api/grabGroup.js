d2d0e0915de907593596ed4f8446d11fe7f2a806
Yan Sharma <yawning008@gmail.com>
2026-03-03 10:57:08 -0500

    made grab group case insensitive

--- app/server/api/grabGroup.js
+++ app/server/api/grabGroup.js
@@ -8,13 +8,18 @@ export default defineEventHandler(async (event) => {
 
   //find group by leader name osis and email
   const { firstName, lastName, email, osis } = body.leader;
-  const group = await Group.findOne({ "leader.firstName":firstName,"leader.lastName":lastName,"leader.email":email, "leader.osis":osis});
+  const group = await Group.findOne({ 
+    "leader.firstName": firstName,
+    "leader.lastName": lastName,
+    "leader.email": email,
+    "leader.osis": osis
+  }).collation({ locale: 'en', strength: 2 });  
   if (!group) {
-    throw createError({
-      statusCode: 404,
-      message:
-        "Group not found, check if your information is correct or if you were really the group leader",
-    });
-  }
-  return group;
+      throw createError({
+        statusCode: 404,
+        message:
+          "Group not found, check if your information is correct or if you were really the group leader",
+      });
+    }
+    return group;
 });
